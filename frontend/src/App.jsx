47e7aa308d30dfcc0ac6028d9ded58eeb8b710b1
eternizers-pixel <eternizers@gmail.com>
2026-05-24 12:07:05 -0300

    feat: rota nova /doacao + /dashboard separado de home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
+import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
-import Estoque from './pages/Estoque';
+import Estoque from './pages/Estoque';     // tela "Realizar Doação"
 import Brindes from './pages/Brindes';
 import Movimentacoes from './pages/Movimentacoes';
 import Relatorios from './pages/Relatorios';
@@ -10,11 +11,13 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index                  element={<Dashboard />} />
-        <Route path="estoque"         element={<Estoque />} />
-        <Route path="brindes"         element={<Brindes />} />
-        <Route path="movimentacoes"   element={<Movimentacoes />} />
-        <Route path="relatorios"      element={<Relatorios />} />
+        <Route index                element={<Home />} />
+        <Route path="doacao"        element={<Estoque />} />
+        <Route path="estoque"       element={<Navigate to="/doacao" replace />} />
+        <Route path="brindes"       element={<Brindes />} />
+        <Route path="dashboard"     element={<Dashboard />} />
+        <Route path="movimentacoes" element={<Movimentacoes />} />
+        <Route path="relatorios"    element={<Relatorios />} />
       </Route>
     </Routes>
   );
